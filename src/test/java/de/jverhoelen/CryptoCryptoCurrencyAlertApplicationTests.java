34d95f668d46e314a9c65f16d919f0008d52a2ec
Verhoelen Jonas <jonas.verhoelen@codecentric.de>
2017-05-20 21:41:34 +0200

    provided mocking of SlackSession for unit tests w/ spring context

--- src/test/java/de/jverhoelen/CryptoCryptoCurrencyAlertApplicationTests.java
+++ src/test/java/de/jverhoelen/CryptoCryptoCurrencyAlertApplicationTests.java
@@ -1,18 +1,22 @@
 package de.jverhoelen;
 
+import com.ullink.slack.simpleslackapi.SlackSession;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = {CryptoCurrencyAlertApplication.class})
 @ActiveProfiles("test")
 public class CryptoCryptoCurrencyAlertApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @MockBean
+    private SlackSession slackSession;
 
+    @Test
+    public void contextLoads() {
+    }
 }
